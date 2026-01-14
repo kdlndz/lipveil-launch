63676758c85465c49a7d5f1bf719275be9019c4f
kdlndz <kailadolandeza@gmail.com>
2026-01-14 16:00:34 +0800

    Edit image url and edit css

---
+++ script.js
@@ -0,0 +1,13 @@
+/*window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    var navbar = document.getElementById("navbar-logo");
+
+    if(document.body.scrollTop > 20 || document.documentElement > 20){
+        navbar.classList.add("show");
+    } else {
+        navbar.classList.remove("show");
+    }
+}*/
