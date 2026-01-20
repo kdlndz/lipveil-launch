fd8bf2f5606446b34a64aea9bd180b8412c179b3
kdlndz <kailadolandeza@gmail.com>
2026-01-20 19:50:31 +0800

    Add onclick command for join waitlist section

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-/*window.onscroll = function() {
-    scrollFunction()
-};
-
-function scrollFunction() {
-    var navbar = document.getElementById("navbar-logo");
-
-    if(document.body.scrollTop > 20 || document.documentElement > 20){
-        navbar.classList.add("show");
-    } else {
-        navbar.classList.remove("show");
+function goToRegistration() {
+    const targetElement = document.getElementById("emailReg");
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
     }
-}*/
+}
